21afe2fdfea379ebebc66063dbf997c753d8ec64
Darius Hall <largerest@gmail.com>
2025-08-03 12:20:20 -0500

    feat: initialize SvelteKit project with Tailwind CSS

--- src/consts.ts
+++
@@ -1,4 +0,0 @@
-
-export const SITE_TITLE = 'Largerest';
-export const SITE_DESCRIPTION = "A personal blog and portfolio site.";
-export const SITE_URL = 'https://largerest.com';
--- src/pages/api.ts
+++
@@ -1,5 +0,0 @@
-export function GET(context: any) {
-  return new Response(JSON.stringify({ message: "Hello from API!" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
